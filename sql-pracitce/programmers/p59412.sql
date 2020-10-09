70371e4351b0a34135dfbd94b23dce933ca068b7
siyoon <siyoon210@gmail.com>
2020-10-10 04:25:57 +0900

    프로그래머스 59412번

---
+++ sql-pracitce/programmers/p59412.sql
@@ -0,0 +1,6 @@
+--https://programmers.co.kr/learn/courses/30/lessons/59412
+SELECT hour(DATETIME) AS HOUR, count(hour(DATETIME)) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+HAVING HOUR BETWEEN 9 AND 19
+ORDER BY HOUR
